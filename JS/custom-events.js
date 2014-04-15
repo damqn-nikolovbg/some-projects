21237e0c944015aad68ce1b7344e115066d1a6b9
damqn-nikolovbg <damqn_nikolovbg@abv.bg>
2014-04-15 10:25:57 +0300

    Added some of my projects
    just added some of my projects, demos, etc.

---
+++ JS/custom-events.js
@@ -0,0 +1,20 @@
+﻿var custom = new CustomEvent("clickTreeTimes");
+
+var body = document.getElementsByTagName("body")[0];
+
+body.addEventListener("clickTreeTimes", function () {
+    alert("You triger custom event!");
+}, false);
+
+var getButton = document.getElementById("button");
+
+var counter = 0;
+getButton.addEventListener('click', function () {
+
+    counter++;
+    if (counter == 3) {
+        // Triger Custom Event when condition is present
+        counter = 0;
+        body.dispatchEvent(custom);
+    }
+}, false);
